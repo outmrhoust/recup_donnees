5c1086bd716cd7b31cf619b4c61d4839b634db40
clayyre <claire.guerrini2@hotmail.fr>
2024-02-06 14:19:20 +0100

    good

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 /* This is importing the modules that we need to use in our project. */
-const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 const nmea = require('@drivetech/node-nmea')
 const {InfluxDB, Point} = require('@influxdata/influxdb-client')
 
-const app = express();
 
 const token = process.env.INFLUXDB_TOKEN
 const url = 'http://localhost:8086'
-
-
-
-
-/* Parsing the body of the request. */
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-/* Telling the server to use the public folder as a static folder. */
-app.use(express.static("public"));
 
 
 
@@ -29,16 +14,14 @@ let bucket = `new_test`
 const client = new InfluxDB({url, token})
 const  writeClient = client.getWriteApi(org, bucket, 'ns')
 
-// console.log(dataGPS.loc.geojson.coordinates)
-// console.log(dataRain)
-// console.log(`${dataGPS.datetime.toJSON()}`)
-// console.log(new Date(dataGPS.datetime))
 
 function writeFileToDB () {
+
 
   const data = JSON.parse(fs.readFileSync('./data/sensors', 'utf8'))
   const dataGPS = nmea.parse(fs.readFileSync('./data/gpsNmea', 'utf8').split('\n')[1])
   const dataRain = fs.readFileSync('./data/rainCounter.log', 'utf8').split('\n')[0]
+  console.log(data.date)
 
   const point = new Point('weather')
     .tag('station', "piensg031")
@@ -60,52 +43,29 @@ function writeFileToDB () {
   writeClient.flush()
   console.log("success")
 
-}
+  let queryClient = client.getQueryApi(org)
+  let fluxQuery = `from(bucket: "new_test")
+  |> range(start: -30d)
+  |> last()
+  |> filter(fn: (r) => r._measurement == "weather")
+  |> filter(fn: (r) => r._field == "date_lastrain")
+  `
 
-writeFileToDB()
-while (true) {
+  queryClient.queryRows(fluxQuery, {
+    next: (row, tableMeta) => {
+      const tableObject = tableMeta.toObject(row)
+      console.log(tableObject)
+    },
+    error: (error) => {
+      console.error('\nError', error)
+    },
+    complete: () => {
+      console.log('\nSuccess')
+    },
+  })
 
   setTimeout(writeFileToDB,30000)
 
 }
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let queryClient = client.getQueryApi(org)
-// let fluxQuery = `from(bucket: "new_test")
-//  |> range(start: -30d)
-//  |> last()
-// |> filter(fn: (r) => r._measurement == "weather")
-// |> filter(fn: (r) => r._field == "date_location")
-// `
-
-// queryClient.queryRows(fluxQuery, {
-//   next: (row, tableMeta) => {
-//     const tableObject = tableMeta.toObject(row)
-//     console.log(tableObject)
-//   },
-//   error: (error) => {
-//     console.error('\nError', error)
-//   },
-//   complete: () => {
-//     console.log('\nSuccess')
-//   },
-// })
-
-// /* Starting the server. */
-// app.listen(process.env.PORT || 4000, function () {
-//   console.log("Server started");
-// });
+writeFileToDB()
